712ca31fd3a53f945e70ea86d2ef2989004bd2a0
Morten Siebuhr <msi@one.com>
2013-05-08 13:33:35 +0200

    Test: Have the simple test actually check the output.

--- test/simple.js
+++ test/simple.js
@@ -3,8 +3,10 @@
 
 var assert = require('chai').assert,
     express = require('express'),
-    request = require('supertest')
-    har = require('../index.js');
+    fs = require('fs'),
+    har = require('../index.js'),
+    path = require('path'),
+    request = require('supertest');
 
 describe('Simple test', function () {
     var app;
@@ -13,10 +15,25 @@ describe('Simple test', function () {
         app = express();
 
         app.use(har({
+            harOutputDir: __dirname
         }));
 
         app.get('/', function (req, res, next) {
             return res.send(200, 'This is quite OK');
+        });
+    });
+
+    // Remove *.har-files after each test
+    afterEach(function (done) {
+        fs.readdir(__dirname, function (err, files) {
+            files
+            .filter(function (filename) {
+                return filename.indexOf('.har') > 10;
+            })
+            .forEach(function (filename) {
+                fs.unlinkSync(path.join(__dirname, filename));
+            });
+            done(err);
         });
     });
 
@@ -26,8 +43,28 @@ describe('Simple test', function () {
             .set('Custom-header', 'foo/bar')
             .expect(200)
             .end(function (err, res) {
-                // TODO: Check emitted HAR-file
-                done(err);
+                // Wait for file to be written to disk
+                setTimeout(function () {
+                    var filename = fs.readdirSync(__dirname).filter(function (filename) {
+                        return filename.indexOf('.har') > 10;
+                    })[0];
+
+                    // It is valid JSON
+                    var json;
+                    try {
+                        var fullFilename = path.join(__dirname, filename),
+                            data = fs.readFileSync(fullFilename);
+                        json = JSON.parse(data);
+                    } catch (e) {
+                        assert(e, 'Could not parse JSON');
+                        return done(e);
+                    }
+
+                    // Simple sanity check
+                    assert.deepProperty(json, 'log.entries.0');
+
+                    done(err);
+                }, 5);
             });
     });
 });
